6c9ad5dbc4b6de979c300ff580c1567adbfa8db5
zdziwionyrysiczeq <54484477+mrchomik892@users.noreply.github.com>
2019-12-05 18:37:35 +0100

    Create main python file

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import time
+print "Sad Lynx Libary Alpha";
+time.sleep(0.25);
+print "Now, nothing to work";
+print "Nothing is maked";
+print "Time to Hello World";
+time.sleep(0.9);
+print "Hello World!"; # Hello World Script
+exit(5);
